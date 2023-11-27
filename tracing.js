9a9f024f6282c69481a3ce375503d943fe5268c1
erikhartker <erik.hartker@ufl.edu>
2023-11-26 22:48:39 -0500

    Clean up tracing.js

--- tracing.js
+++ tracing.js
@@ -13,53 +13,6 @@ const { BatchSpanProcessor } = require('@opentelemetry/sdk-trace-base');
 
 const { JaegerExporter } = require('@opentelemetry/exporter-jaeger');
 
-// module.exports = (serviceName) => {
-//     const options = {
-//     tags: [], // optional
-//     // You can use the default UDPSender
-//     host: 'localhost', // optional
-//     port: 3000, // optional
-//     // OR you can use the HTTPSender as follows
-//     // endpoint: 'http://localhost:14268/api/traces',
-//     maxPacketSize: 65000 // optional
-//     }
-//     const exporter = new JaegerExporter(options);
-//     const provider = new NodeTracerProvider();
-//     provider.addSpanProcessor(new BatchSpanProcessor(exporter));
-//     provider.register();
-//     return provider.getTracer("todo-service");
-// };
-
-
-// const options = {
-//   serviceName: 'todo-service',
-//   tags: [], // optional
-//   // You can use the default UDPSender
-//   host: 'localhost', // optional
-//   port: 3000, // optional
-//   // OR you can use the HTTPSender as follows
-//   // endpoint: 'http://localhost:14268/api/traces',
-//   maxPacketSize: 65000 // optional
-// }
-// const exporter = new JaegerExporter(options);
-// const provider = new NodeTracerProvider({
-//     resource: new Resource({
-//         [SemanticResourceAttributes.SERVICE_NAME]: 'todo-service',
-//     }),
-// });
-// provider.addSpanProcessor(new BatchSpanProcessor(exporter));
-// provider.register();
-// registerInstrumentations({
-//     instrumentations: [
-//         new HttpInstrumentation(),
-//         new ExpressInstrumentation(),
-//         new MongoDBInstrumentation(),
-//     ],
-//     tracerProvider: provider,
-// });
-// module.exports = trace.getTracer('todo-service');
-
-
 //Exporter
 // module.exports = (serviceName) => {
 //    const exporter = new ConsoleSpanExporter();
